f759ddc0aebcff42e861a9751eef1d000f7872e6
Gabriel <60533817+GabrielGarcoaRodrigues@users.noreply.github.com>
2025-06-13 13:54:13 -0300

    Delete infra/backend.tf

--- infra/backend.tf
+++
@@ -1,9 +0,0 @@
-
-terraform {
-  backend "s3" {
-    bucket         = "meu-bucket-de-estado-terraform-unico" 
-    key            = "global/s3/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks" 
-  }
-}
