ea3a2c84a74a7f3bf8bbf2de1c0b255da057f60a
Gabriel <60533817+GabrielGarcoaRodrigues@users.noreply.github.com>
2025-06-13 13:05:11 -0300

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,9 +1,9 @@
 
 terraform {
   backend "s3" {
-    bucket         = "meu-bucket-de-estado-terraform-unico" # IMPORTANTE: Escolha um nome único globalmente!
+    bucket         = "meu-bucket-de-estado-terraform-unico" 
     key            = "global/s3/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-locks" # Tabela para evitar conflitos
+    dynamodb_table = "terraform-locks" 
   }
 }
