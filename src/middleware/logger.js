81f64c92f6e1e5cc147d6077cc68050f64cd1ea5
Gabriel <60533817+GabrielGarcoaRodrigues@users.noreply.github.com>
2025-06-13 14:25:42 -0300

    Create logger.js

---
+++ src/middleware/logger.js
@@ -0,0 +1,32 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.errors({ stack: true }),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.simple()
+    })
+  ]
+});
+
+module.exports = (req, res, next) => {
+  const start = Date.now();
+  
+  res.on('finish', () => {
+    const duration = Date.now() - start;
+    logger.info({
+      method: req.method,
+      url: req.url,
+      status: res.statusCode,
+      duration: `${duration}ms`,
+      userAgent: req.get('User-Agent')
+    });
+  });
+  
+  next();
+};
